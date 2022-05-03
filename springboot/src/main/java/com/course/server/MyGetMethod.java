e9a18d58a774161a9fc6bbf8816d3c80347e396e
yuanlai159911420@163.com <yuanlai159911420@icloud.com>
2022-05-03 14:36:48 +0800

    文件修改

---
+++ springboot/src/main/java/com/course/server/MyGetMethod.java
@@ -0,0 +1,14 @@
+package com.course.server;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyGetMethod {
+
+    @RequestMapping(value = "/getCookies", method = RequestMethod.GET)
+    public String getCookies(){
+        return "恭喜你获取cookies成功";
+    }
+}
